6c8a0253483383f8f88a238572f02627f2f568fd
Amaury Carrade <amaury@carrade.eu>
2015-10-31 18:21:54 +0100

    Plugin's structure.
    * NEW: added an empty listener.
    * NEW: added config.

--- src/main/java/fr/zcraft/PositionalChat/PositionalChat.java
+++ src/main/java/fr/zcraft/PositionalChat/PositionalChat.java
@@ -7,11 +7,34 @@ public class PositionalChat extends JavaPlugin
 {
     private static PositionalChat instance;
 
+    private int CLEAR_BEFORE;
+    private int OBFUSCATED_AFTER;
+
+
     @Override
     public void onEnable()
     {
         instance = this;
+
+        saveDefaultConfig();
+
+        CLEAR_BEFORE     = getConfig().getInt("distances.clearBefore", 30);
+        OBFUSCATED_AFTER = getConfig().getInt("distances.obfuscatedAfter", 150);
+
+        getServer().getPluginManager().registerEvents(new ChatListener(), this);
     }
+
+
+    public int getClearBefore()
+    {
+        return CLEAR_BEFORE;
+    }
+
+    public int getObfuscatedAfter()
+    {
+        return OBFUSCATED_AFTER;
+    }
+
 
     public static PositionalChat get()
     {
